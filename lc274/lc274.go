b1aad68ee527389c86d8dae3aad03d5989118627
Carlisle <carlisleshawxyz@gmail.com>
2019-02-26 16:00:02 +0800

    improved lc274

--- lc274/lc274.go
+++ lc274/lc274.go
@@ -5,6 +5,12 @@ import (
 	"sort"
 )
 
+// 思路：排序数组，遍历
+// if 第i个元素的值 小于等于 第i个元素后的元素个数（含第i个元素）
+//    if 该值等于 第i个元素后的元素个数（含第i个元素）或 第i个元素后的元素个数（不含第i个元素）
+//       该值即为h指数
+// else 第i个元素的值 大于 第i个元素后的元素个数（含第i个元素） 例如：[100] 或 [1,100,101] 等情况
+//		取h指数为 第i个元素后的元素个数（含第i个元素）
 func hIndex(citations []int) int {
 	sort.Ints(citations)
 	for i := 0; i < len(citations); i++ {
@@ -13,11 +19,7 @@ func hIndex(citations []int) int {
 				return citations[i]
 			}
 		} else {
-			for j := 1; (i > 0 && citations[i]-j >= citations[i-1]) || i == 0; j++ {
-				if citations[i]-j == len(citations)-i {
-					return citations[i] - j
-				}
-			}
+			return len(citations) - i
 		}
 	}
 	return 0
---
+++ lc275/lc275.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func hIndex(citations []int) int {
+	// p := len(citations) / 2
+
+	return 0
+}
+
+func main() {
+	citations := []int{0, 1, 3, 5, 6}
+	ans := hIndex(citations)
+	fmt.Println(ans)
+}
