86644f0a8d57217895c2e1f51c0b9fff7a428b27
Carlisle <carlisleshawxyz@gmail.com>
2019-03-09 14:54:55 +0800

    lc24

---
+++ lc24/lc24.go
@@ -0,0 +1,26 @@
+package main
+
+import . "LeetCode/ListNode"
+
+func swapPairs(head *ListNode) *ListNode {
+	tmphead := &ListNode{Val: 0, Next: head}
+	tmp := tmphead
+	for tmp.Next != nil && tmp.Next.Next != nil {
+		a := tmp.Next
+		b := a.Next
+		c := b.Next
+		tmp.Next = b
+		b.Next = a
+		a.Next = c
+		tmp = tmp.Next.Next
+	}
+	return tmphead.Next
+}
+
+func main() {
+	head := MakeList([]int{1, 2, 3, 4, 5, 6, 7})
+	// head := MakeList([]int{1})
+	PrintList(head)
+	swaphead := swapPairs(head)
+	PrintList(swaphead)
+}
