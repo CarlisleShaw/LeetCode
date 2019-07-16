d7384c228fdb431e4e0b2d1958d5b52f7dbb97ea
Carlisle <carlisleshawxyz@gmail.com>
2019-07-16 17:04:07 +0800

    add .gitignore

---
+++ lc22/Solution.java
@@ -0,0 +1,16 @@
+package LeetCode.lc22;
+
+import java.util.*;
+import java.lang.*;
+
+class Solution {
+
+    public List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList<String>();
+        
+        return list;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
