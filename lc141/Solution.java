43185f03fac948707e61747ab0e1bd8a3e5df76c
Carlisle <carlisleshawxyz@gmail.com>
2019-07-16 19:36:24 +0800

    rename

---
+++ lc141/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        return true;
+    }
+}
+
+private class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
