e348464fe49cb6788acef0365b33a1cd8dac9e3b
Carlisle <carlisleshawxyz@gmail.com>
2019-07-16 19:04:51 +0800

    update the comment

--- lc206/lc206.java
+++ lc206/lc206.java
@@ -1,3 +1,20 @@
+/****************************************
+Reverse Linked List
+When a chain is being reversed, it will be disconnected into 2 chains.
+The head node of 2 chains needs to be recorded as 'prev' & 'cur'.
+Before disconnecting 'cur' & 'cur.next', 'cur.next' needs to be recorded as 'next'.
+           prev  cur   next
+             |    |   /
+origin:     NULL  1->2->3->4->5->NULL
+processing: NULL<-1<-2  3->4->5->NULL
+                    /   |   \
+                prev   cur   next
+Operations：
+1.cur.next->next
+2.connect prev & cur
+3.disconnect cur & next
+4.move prev & cur 1 position to the right 
+****************************************/
 class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode cur = head;
